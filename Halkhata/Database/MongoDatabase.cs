c6b5cb4c909e980ac5dc816c6feb23ce842b099d
Anabilsarker <sarkeranabil@gmail.com>
2022-12-15 16:33:37 +0600

    Add project files.

---
+++ Halkhata/Database/MongoDatabase.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halkhata.Database
+{
+    public static class MongoDatabase
+    {
+        static MongoClient dbClient;
+        public static void InitMongo()
+        {
+            dbClient = new MongoClient("mongodb://localhost:27017/");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
---
+++ Halkhata/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Halkhata.Database;
+using Halkhata.UC;
+using MaterialDesignThemes.Wpf;
+
+namespace Halkhata
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Instance { get { return _instance; } }
+        private static MainWindow _instance = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _instance = this;
+            MongoDatabase.InitMongo();
+        }
+    }
+}
